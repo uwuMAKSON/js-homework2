ce35e14218e61f2d9fa01782c5b64204ca1da096
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-01-20 16:27:11 +0600

    added script.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// let result = 0;
+// let answer1 = confirm("what is the official name of JavaScript?");
+
+// if (answer1){
+// result = result + 5;
+// } let result = 0;
+
+// let answer1 = confirm("what HTML stands for?");
+
+// if (answer1 == "Hyper Text Markup Language"){
+// result = result + 5; 
+// } 
+// let result = 0;
+// let answer1 = prompt("which HTML tag is used for paragraphs?");
+
+// if (answer1 =="p"){
+// result = result + 5;
+// }
+
+
+//  //Show results 
+//  console.log("You've earned " + result + "points!")
