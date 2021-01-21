5d4954258c6292413830944dff78bf2c24ea6170
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-01-21 12:54:23 +0600

    added second question

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
-let result = 0;
+let result = 0 //10
+let result1 = 0;
 let answer1 = confirm("what is the official name of JavaScript?");
 
-if (answer1){
-    result = result + 5;
+if (answer1) {
+    result = result1 + 10;
 };
 
-// let result = 0;
-// let answer2 = confirm("which HTML tag is used for paragraphs?");
+let result2 = 10;
+let answer2 = confirm("which HTML tag is used for paragraphs?");
 
-// if (answer2){
-//     result = result + 5;
-// }
+if (answer2) {
+    result = result2 + 10;
+}
+
+
+
+
 
 //Show results 
-console.log("You've earned " + result + "points!");
+console.log("You've earned " + result + " points!");
