a39d68f07ae9d9f19000dc236b985b19de6ea22b
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-01-21 12:56:43 +0600

    added third question

--- scripts.js
+++ scripts.js
@@ -4,13 +4,20 @@ let answer1 = confirm("what is the official name of JavaScript?");
 
 if (answer1) {
     result = result1 + 10;
-};
+}
 
 let result2 = 10;
 let answer2 = confirm("which HTML tag is used for paragraphs?");
 
 if (answer2) {
     result = result2 + 10;
+}
+
+let result3 = 10;
+let answer3 = confirm("what is the official name of JavaScript?");
+
+if (answer3) {
+    result = result2 + result3 + 10;
 }
 
 
